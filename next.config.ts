a7658ea2a8de931fc2fe5c8b0c32a4a26eb9e408
Adrian Teh <adrianteh02@hotmail.com>
2024-11-17 18:45:43 +0800

    feat: portfolio v1

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const env = process.env.ENV ?? "production";
+console.log("process.env.ENV", process.env.ENV, typeof process.env.ENV);
+
+console.log("env: ", env);
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/adrianteh126.github.io",
+  basePath: env === "production" ? "/adrianteh126.github.io" : "",
   output: "export",
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
+import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
@@ -14,5 +16,39 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addUtilities, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.5xl") },
+        h2: { fontSize: theme("fontSize.4xl") },
+        h3: { fontSize: theme("fontSize.3xl") },
+        h4: { fontSize: theme("fontSize.2xl") },
+        h5: { fontSize: theme("fontSize.xl") },
+        h6: { fontSize: theme("fontSize.lg") },
+        p: { fontSize: theme("fontSize.base") },
+      }),
+        addUtilities({
+          /* Hide scrollbar for Chrome, Safari and Opera */
+          ".no-scrollbar::-webkit-scrollbar": {
+            display: "none",
+          },
+          /* Hide scrollbar for IE, Edge and Firefox */
+          ".no-scrollbar": {
+            "-ms-overflow-style": "none" /* IE and Edge */,
+            "scrollbar-width": "none" /* Firefox */,
+          },
+        });
+    }),
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light"], // daisyUI themes
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 } satisfies Config;
